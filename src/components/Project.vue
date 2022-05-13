16b8b00e0421527845366f26f19db284ae83f991
Hugo REY <hrey222@gmail.com>
2022-05-13 12:51:33 +0200

    project card blurry +v1 effect

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,40 +1,55 @@
 <template >
-    <div class="gradient-background">
+    <div>
+        <div class="gradient-background">
+            {{side}}
+            <h2 class="title">{{project[this.$store.state.lang].title}}</h2>
+            <p class="date">{{project[this.$store.state.lang].date}}</p>
+            <p class="description">{{project[this.$store.state.lang].description}}</p>
+            <button>{{this.$store.state.txt[this.$store.state.lang]["discoverBtn"]}}</button>
+        </div>
+        <img src="@/assets/img/" :alt="project[this.$store.state.lang].title">
     </div>
 </template>
 <script>
 export default {
     name: 'Project',
+    props: ['project', 'side'],
 }
 </script>
 <style lang="scss" scoped>
     .gradient-background {
         width: 300px;
         height: 300px;
-        
-        opacity: 0.5;
+
+        border-radius: 20px;
+
+        transition: background-image 1.5s ease-in-out;
+        background-image: url('~@/assets/img/blurry.png');
+        background-position: 0 0;
+        background-size: cover;
+        background-repeat: no-repeat;
+
+        background-color: hsla(0,0%,100%,.15);
+        -webkit-backdrop-filter: blur(10px);
+        backdrop-filter: blur(10px);
         &:hover{
             background-image: url('~@/assets/img/gradient.jpg');
-            background-position: 0 0;
-            background-size: cover;
-            background-repeat: no-repeat;
-            animation: gradient-animation 10s ease alternate-reverse infinite;
+            animation: gradient-animation 20s ease alternate-reverse infinite;
         }
-        background-color: $dark;
-        transition: all 0.5s ease;
+        
     }
 
     @keyframes gradient-animation {
     0% {
-        background-position: 0% 50%;
-        opacity: 0.2;
+        opacity: 0;
     }
-    50% {
-        background-position: 100% 50%;
+    2%{
+        opacity: 0.5;
+    }
+    10%,45%,100% {
         opacity: 0.8;
     }
-    100% {
-        background-position: 0% 50%;
+    30%, 60%, 80% {
         opacity: 0.2;
     }
     }
